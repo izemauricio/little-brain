6d78d9688239d43d27086e177a6af0b427d5468d
William Barbsoa <williampmb@gmail.com>
2018-09-13 12:44:16 -0300

    ADDED: - grow food

--- food.js
+++ food.js
@@ -2,17 +2,19 @@ function Food() {
     this.x = random(1,width);
     this.y = random(1,height);
     this.position = createVector(this.x,this.y);
-    this.diameter = random(10, 30);
-    this.speed = 0.2;
+    this.diameter = 1;
     this.r = random(0,50);
-    this.g = random(0,255);
-    this.b = random(0,50);
     this.energy = random(50,300);
     this.pregnant = false;
     this.dead = false;
     this.age = random(10,500);
-    this.toxity = 1;
+    this.nutrition = random(1,9);
+
+
+    this.g = random(0,255);
+    this.b = random(0,50);
     this.maxenergy = random(20,290);
+    this.toxity = 1;
 
     this.move = function() {
 
@@ -35,10 +37,10 @@ function Food() {
       drawingContext.shadowBlur = 2;
       drawingContext.shadowColor = "green";
 
-        fill(this.toxity,255,0);
-        ellipse(this.position.x, this.position.y, this.age/8, this.age/8);
+      fill(this.toxity,255,0);
+      ellipse(this.position.x, this.position.y, this.nutrition, this.nutrition);
 
-        drawingContext.shadowBlur = 0;
+      drawingContext.shadowBlur = 0;
       drawingContext.shadowColor = "black";
     };
   }
@@ -56,12 +58,32 @@ function Food() {
     }
   }
 
+  Food.prototype.grow = function(index){
+    for(var i = 0 ; i<foods.length; i++){
+      if(i == index) continue;
+
+      var distance = p5.Vector.sub(foods[i].position, this.position);
+      if(distance.mag() < foods[i].nutrition/2 + this.nutrition/2 +1){
+        return;
+      }
+    }
+    if(this.nutrition <50){
+        this.nutrition +=0.5;
+    }
+  }
+
+  Food.prototype.outOfBoundaries = function(){
+    var raio = this.nutirion/2;
+
+  }
+
 
   Food.prototype.toBehave = function(index){
     this.move();
     this.draw();
     this.checkdeath();
     this.checkpregnant();
+    this.grow(index);
     if(this.dead) {
         //foods.splice(index,1);
         return;
