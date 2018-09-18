011b8d7d036ae1e7835f7b2d40d4bb0c887d002e
Maurici Ize <mauriciodcjz@gmail.com>
2018-09-18 15:34:39 -0300

    format

--- body.js
+++ body.js
@@ -20,21 +20,21 @@ class Sensor {
     }
 }
 
- // body sensor setup
- // How many sensors does each vehicle have?
- var totalSensors = 8;
- // How far can each vehicle see?
- var sensor_power = 150;
- // What's the angle in between sensors
- var sensorAngle = (Math.PI * 2) / totalSensors;
+// body sensor setup
+// How many sensors does each vehicle have?
+var totalSensors = 8;
+// How far can each vehicle see?
+var sensor_power = 150;
+// What's the angle in between sensors
+var sensorAngle = (Math.PI * 2) / totalSensors;
 
 class Body {
-    constructor(x,y) {
+    constructor(x, y) {
         // brain
         //this.brain = new NeuralNetwork(inputs, 32, 2);
 
         // position and size
-        this.position = createVector(x,y);
+        this.position = createVector(x, y);
         this.r = 5;
 
         // movement
@@ -47,7 +47,7 @@ class Body {
         // status
         this.health = maxlife;
         this.age = 0;
-        this.score =0;
+        this.score = 0;
 
         // shoot
         this.firerate = 0;
@@ -60,17 +60,15 @@ class Body {
         for (let angle = 0; angle < (Math.PI * 2); angle += sensorAngle) {
             this.sensors.push(new Sensor(angle));
         }
-
         // All sensors start with maximum length
         for (let j = 0; j < this.sensors.length; j++) {
             this.sensors[j].val = sensor_power;
         }
 
-        this.brain = new NeuralNetworkW(this.sensors.length, 32 , 2);
+        this.brain = new NeuralNetworkW(this.sensors.length, 32, 2);
 
         //debug mode
-        this.outputDebug =[];
-
+        this.outputDebug = [];
     }
 
     toBehave(index) {
@@ -89,50 +87,50 @@ class Body {
 
     }
 
-    think(){
+    think() {
 
-      let inputs = [];
+        let inputs = [];
 
-      //velocity x and y normalized 0~1
-      let velocityX = this.velocity.x / this.maxspeed;
-      let velocityY = this.velocity.y / this.maxspeed;
+        //velocity x and y normalized 0~1
+        let velocityX = this.velocity.x / this.maxspeed;
+        let velocityY = this.velocity.y / this.maxspeed;
 
-      //sensors normalized 0~1
-      for(var i = 0 ; i < this.sensors.length; i++){
-        inputs.push(map(this.sensors[i].val, 0, sensor_power, 1,0));
-      }
-      let output = this.brain.supletivo(inputs);
-      this.outputDebug = output;
+        //sensors normalized 0~1
+        for (var i = 0; i < this.sensors.length; i++) {
+            inputs.push(map(this.sensors[i].val, 0, sensor_power, 1, 0));
+        }
+        let output = this.brain.supletivo(inputs);
+        this.outputDebug = output;
 
-      //Turn the output as a vector with low intensity
-      let desired = createVector(output[0]*2-1, output[1]*2-1);
-      //Give power to the direction
-      desired.mult(this.maxspeed);
-      // Craig Reynolds steering formula, smoothing the path
-      let steer = p5.Vector.sub(desired, this.velocity);
-      steer.limit(this.maxforce);
-      //Apply the force
-      this.applyForce(steer);
+        //Turn the output as a vector with low intensity
+        let desired = createVector(output[0] * 2 - 1, output[1] * 2 - 1);
+        //Give power to the direction
+        desired.mult(this.maxspeed);
+        // Craig Reynolds steering formula, smoothing the path
+        let steer = p5.Vector.sub(desired, this.velocity);
+        steer.limit(this.maxforce);
+        //Apply the force
+        this.applyForce(steer);
     }
 
     // Check against array of food
     eat(list) {
-      for (let i = list.length - 1; i >= 0; i--) {
-        // Calculate distance
-        let d = p5.Vector.dist(list[i].position, this.position);
-        // If vehicle is within food radius, eat it!
-        if (d < list[i].nutrition/2) {
-          this.health += list[i].nutrition/2;
-          list.splice(i, 1);
+        for (let i = list.length - 1; i >= 0; i--) {
+            // Calculate distance
+            let d = p5.Vector.dist(list[i].position, this.position);
+            // If vehicle is within food radius, eat it!
+            if (d < list[i].nutrition / 2) {
+                this.health += list[i].nutrition / 2;
+                list.splice(i, 1);
+            }
         }
-      }
     }
 
     move() {
         this.velocity.add(this.acceleration);
         this.velocity.limit(this.maxspeed);
         if (this.velocity.mag() < this.minspeed) {
-          this.velocity.setMag(this.minspeed);
+            this.velocity.setMag(this.minspeed);
         }
         this.position.add(this.velocity);
         this.acceleration.mult(0);
@@ -144,8 +142,8 @@ class Body {
         this.score += 1;
     }
 
-    applyForce(force){
-      this.acceleration.add(force);
+    applyForce(force) {
+        this.acceleration.add(force);
     }
 
     grow() {
@@ -177,14 +175,15 @@ class Body {
     }
 
     reproduce(prob) {
-      // Pick a random number
-      let rand = random(1);
-      // New vehicle with brain copy, otherwise return null;
-      return rand < prob ? new Body(this.position.x,this.position.y) : null;
+        // Pick a random number
+        let rand = random(1);
+
+        // New vehicle with brain copy, otherwise return null;
+        return rand < prob ? new Body(this.position.x, this.position.y) : null;
     }
 
     isDead() {
-      return this.health <= 0;
+        return this.health <= 0;
     }
 
     createforce(bodies, foods, bullets) {
@@ -203,9 +202,9 @@ class Body {
         this.force = steer;
         */
         let mouse = createVector(10, y);
-        this.force = p5.Vector.sub(mouse,this.position);
+        this.force = p5.Vector.sub(mouse, this.position);
         // MOUSE MODE
-      //  let mouse = createVector(mouseX, mouseY);
+        //  let mouse = createVector(mouseX, mouseY);
         //this.force = p5.Vector.sub(mouse,this.position);
 
         // GOD MODE
@@ -267,13 +266,13 @@ class Body {
 
     updateSensors(foods) {
         // for each sensor, check if there is a food in the sensor fov
-        for (let i=0; i<this.sensors.length; i++) {
+        for (let i = 0; i < this.sensors.length; i++) {
             let mysensor = this.sensors[i];
 
             // find the nearest food that is the sensor field of view
             let nearestFood = null;
             let nearestDist = 9999;
-            for (let j=0; j<foods.length; j++) {
+            for (let j = 0; j < foods.length; j++) {
                 let myfood = foods[j];
 
                 let distance = p5.Vector.dist(this.position, myfood.position);
@@ -299,7 +298,7 @@ class Body {
                 }
             }
 
-            mysensor.val = nearestDist < sensor_power? nearestDist : sensor_power;
+            mysensor.val = nearestDist < sensor_power ? nearestDist : sensor_power;
 
         }
     }
@@ -323,7 +322,7 @@ class Body {
 
             stroke(133);
             noFill();
-            line(myfood.position.x,myfood.position.y,this.position.x,this.position.y);
+            line(myfood.position.x, myfood.position.y, this.position.x, this.position.y);
 
             // normalized vector direction to food
 
@@ -333,8 +332,8 @@ class Body {
                 // If the relative angle of the food is in between the range
                 let delta = this.sensors[j].dir.angleBetween(toFood);
 
-                stroke(255,0,0);
-                line(this.position.x,this.position.y,this.position.x + delta.x * 100,this.position.y + delta.y * 100);
+                stroke(255, 0, 0);
+                line(this.position.x, this.position.y, this.position.x + delta.x * 100, this.position.y + delta.y * 100);
 
                 if (delta < sensorAngle / 2) {
                     // Sensor value is the closest food
@@ -378,10 +377,6 @@ class Body {
     }
 
     draw() {
-        // angle of the vector velocity + 90 graus
-        var theta = this.velocity.heading() + (PI/2);
-
-        push();
 
         if (debug.checked()) {
             this.drawLifeBar();
@@ -390,20 +385,13 @@ class Body {
             text(int(this.score), 10, 0);
         }
 
-        // posiciona the body
-        translate(this.position.x, this.position.y);
-
-        // rotate the body
-        rotate(theta);
-
-
         // draw the body
         this.drawBody();
-
-        pop();
     }
 
     drawLifeBar() {
+        push();
+
         //lerpcolor = Linear Interpolation for Color
         //https://www.youtube.com/watch?v=8uLVnM36XUc
         // todo: smooth between 3 colors: red, yellow and green instead of the whole color spectrum.
@@ -418,14 +406,18 @@ class Body {
         stroke(barHealthColor);
 
         rect(this.position.x + 15, this.position.y, 4, -life);
+
+        pop();
     }
 
     drawSensorLines() {
+        push();
+
         drawingContext.shadowBlur = 5;
         drawingContext.shadowColor = "cyan";
         //fill(255, 255, 255);
         noFill();
-        stroke(255, 255, 255,80);
+        stroke(255, 255, 255, 80);
 
         for (let i = 0; i < this.sensors.length; i++) {
             let val = this.sensors[i].val;
@@ -435,28 +427,42 @@ class Body {
                 //strokeWeight(map(val, 0, sensor_power, 4, 1));
                 //drawingContext.shadowBlur = map(val, 0, sensorLength, 6, 1);
 
-                line(this.position.x, this.position.y, this.position.x + pos.x * val,this.position.y + pos.y * val);
+                line(this.position.x, this.position.y, this.position.x + pos.x * val, this.position.y + pos.y * val);
 
                 //text("VAL: " + val + " - POS: " + position2.x + "," + position2.y, this.position.x, this.position.y + (15 * (i + 1)));
             }
         }
+
+        pop();
     }
 
     drawRangeCircle() {
+        push();
         noFill();
         stroke(200, 0, 0);
         strokeWeight(1);
         drawingContext.shadowBlur = 0;
-
-        ellipse(this.position.x, this.position.y, sensor_power* 2);
+        ellipse(this.position.x, this.position.y, sensor_power * 2);
+        pop();
     }
 
     drawBody() {
+        push();
+
+        // angle of the vector velocity + 90 graus
+        var theta = this.velocity.heading() + (PI / 2);
+
+        // posiciona the body
+        translate(this.position.x, this.position.y);
+
+        // rotate the body
+        rotate(theta);
+
         // color
         drawingContext.shadowBlur = 0;
-        fill(255,255,0);
+        fill(255, 255, 0);
         //noFill();
-        stroke(255,255,255  );
+        stroke(255, 255, 255);
         strokeWeight(1);
 
         // shape
@@ -465,5 +471,7 @@ class Body {
         vertex(-this.r, this.r * 2);
         vertex(this.r, this.r * 2);
         endShape(CLOSE);
+
+        pop();
     }
 }
