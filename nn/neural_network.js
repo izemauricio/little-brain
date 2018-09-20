68eaf220e4c78fba674e4704cc7d460ea10d39de
William Barbosa <william@pathfind.com.br>
2018-09-19 21:40:39 -0300

    ADDED: neural_network.js: - activation functions reLU, leaky

--- nn/neural_network.js
+++ nn/neural_network.js
@@ -5,9 +5,33 @@ function sigmoidw(x){
   return 1 / (1 + Math.exp(-x));
 }
 
+function reLU(x){
+  return x>=0?x:0;
+}
+
+function leaky(x){
+  return x>=0?x:0.01*x;
+}
+
+
 class NeuralNetworkW{
 
   constructor(input_nodes, hidden_nodes, output_nodes){
+
+    if(input_nodes instanceof NeuralNetworkW){
+      let nn = input_nodes;
+      this.input_nodes = nn.input_nodes;
+      this.hidden_nodes = nn.hidden_nodes;
+      this.output_nodes = nn.output_nodes;
+
+      this.weights_ih = nn.weights_ih.copy();
+      this.weights_ho = nn.weights_ho.copy();
+
+      this.bias_h = nn.bias_h.copy();
+      this.bias_o = nn.bias_o.copy();
+
+
+    }else{
       this.input_nodes = input_nodes;
       this.hidden_nodes = hidden_nodes;
       this.output_nodes = output_nodes;
@@ -23,6 +47,8 @@ class NeuralNetworkW{
 
       this.bias_h.randomize();
       this.bias_o.randomize();
+    }
+
   }
 
 
@@ -32,13 +58,25 @@ class NeuralNetworkW{
     let input = Matrix.fromArray(input_array);
     let hidden = Matrix.multiply(this.weights_ih, input);
     hidden.add(this.bias_h);
-    hidden.map(sigmoidw);
+    hidden.map(reLU);
 
     let output = Matrix.multiply(this.weights_ho, hidden);
     output.add(this.bias_o);
-    output.map(sigmoidw);
+    output.map(leaky);
 
     return output.toArray();
+  }
+
+  clone(){
+    return new NeuralNetworkW(this);
+  }
+
+  // Accept an arbitrary function for mutation
+  mutate(func) {
+    this.weights_ih.map(func);
+    this.weights_ho.map(func);
+    this.bias_h.map(func);
+    this.bias_o.map(func);
   }
 
 
